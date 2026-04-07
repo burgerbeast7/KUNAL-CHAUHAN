e19f11a8cfa9256c843017a4319e5c3b62eabf38
kunal chauhan <155293191+burgerbeast7@users.noreply.github.com>
2026-04-07 11:36:35 +0530

    feat: configure dual deployment for github pages and render

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  output: process.env.GITHUB_ACTIONS ? "export" : "standalone",
   images: {
+    unoptimized: !!process.env.GITHUB_ACTIONS, // GitHub Pages requires unoptimized images
     remotePatterns: [
       {
         protocol: "https",
